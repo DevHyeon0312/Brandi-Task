7c1cf510ba651d348203883370d9d65e7e044c32
devhyeon <devhyeon0312@gmail.com>
2021-05-07 04:43:27 +0900

    MyApplication.kt 생성 및 startKoin

---
+++ app/src/main/java/com/devhyeon/kakaoimagesearch/MyApplication.kt
@@ -0,0 +1,23 @@
+package com.devhyeon.kakaoimagesearch
+
+import android.app.Application
+import com.devhyeon.kakaoimagesearch.di.network.NetworkModule
+import com.devhyeon.kakaoimagesearch.di.viewmodel.ViewModelModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidContext(this@MyApplication)
+            modules(
+                listOf(
+                    NetworkModule,
+                    ViewModelModule
+                )
+            )
+        }
+    }
+}
