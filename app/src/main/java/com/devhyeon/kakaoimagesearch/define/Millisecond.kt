b7dda904b68479d8d6962f2470e506d1a86a99c5
devhyeon <devhyeon0312@gmail.com>
2021-05-07 04:14:42 +0900

    define 하여 사용할 상수 추가

---
+++ app/src/main/java/com/devhyeon/kakaoimagesearch/define/Millisecond.kt
@@ -0,0 +1,7 @@
+package com.devhyeon.kakaoimagesearch.define
+
+/** 밀리초 */
+const val MS_500  = 500L
+const val MS_1000 = 1000L
+const val MS_1500 = 1500L
+const val MS_2000 = 2000L
---
+++ app/src/main/java/com/devhyeon/kakaoimagesearch/define/Network.kt
@@ -0,0 +1,5 @@
+package com.devhyeon.kakaoimagesearch.define
+
+/** 네트워크 */
+const val NETWORK_CONNECT = 1
+const val NETWORK_DISCONNECT = -1
