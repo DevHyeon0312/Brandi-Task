0273ecc1ccdfb6a67a14cdd3afea00fa00d72050
devhyeon <devhyeon0312@gmail.com>
2021-05-07 16:44:11 +0900

    Image Load (URL) 추가 , hideKeyboard 추가

--- app/src/main/java/com/devhyeon/kakaoimagesearch/utils/ViewExt.kt
+++ app/src/main/java/com/devhyeon/kakaoimagesearch/utils/ViewExt.kt
@@ -1,7 +1,12 @@
 package com.devhyeon.kakaoimagesearch.utils
 
+import android.content.Context
 import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.ImageView
+import com.bumptech.glide.Glide
 
+/** 뷰를 사용하는 Util */
 fun View.toVisible() {
     this.visibility = View.VISIBLE
 }
@@ -12,4 +17,19 @@ fun View.toGone() {
 
 fun View.toInvisible() {
     this.visibility = View.GONE
+}
+
+/** 이미지 가져오기 */
+fun ImageView.loadImage(url: String) {
+    Glide
+        .with(this)
+        .load(url)
+        .into(this)
+}
+
+/** 키보드 숨기기 */
+fun View.hideKeyboard() {
+    val context = this.context
+    val inputManager = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    inputManager.hideSoftInputFromWindow(this.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
 }
