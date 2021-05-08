38bf8e26ae796e8c9f0cc514d149f713792d09bd
devhyeon <devhyeon0312@gmail.com>
2021-05-08 10:59:45 +0900

    constants 패키지 변경 및 에러코드 추가

---
+++ app/src/main/java/com/devhyeon/kakaoimagesearch/constants/AppError.kt
@@ -0,0 +1,4 @@
+package com.devhyeon.kakaoimagesearch.define.error
+
+const val UNKNOWN_ERROR = 40000
+const val NETWORK_ERROR = 40001
--- app/src/main/java/com/devhyeon/kakaoimagesearch/utils/constants/AppError.kt
+++
@@ -1,3 +0,0 @@
-package com.devhyeon.kakaoimagesearch.define.error
-
-const val UNKNOWN_ERROR = 40001
